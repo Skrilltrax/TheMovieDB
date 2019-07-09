36bd455ae4963abe1e5d76ac1f890d64b36023f4
Aditya Wasan <adityawasan55@gmail.com>
2019-07-09 15:34:36 +0530

    Home Fragment: Use databinding

--- app/src/main/java/me/skrilltrax/themoviedb/ui/homepage/HomeFragment.kt
+++ app/src/main/java/me/skrilltrax/themoviedb/ui/homepage/HomeFragment.kt
@@ -16,6 +16,8 @@ import com.google.android.material.tabs.TabLayout
 import me.skrilltrax.themoviedb.R
 import me.skrilltrax.themoviedb.adapter.ViewPagerAdapter
 import me.skrilltrax.themoviedb.constants.MovieTabs
+import me.skrilltrax.themoviedb.databinding.FragmentCommonViewpagerBinding
+import me.skrilltrax.themoviedb.databinding.FragmentHomeBinding
 import me.skrilltrax.themoviedb.model.movie.lists.MovieResultsItem
 import me.skrilltrax.themoviedb.ui.BaseFragment
 
@@ -23,22 +25,22 @@ class HomeFragment : BaseFragment() {
 
     private lateinit var tabLayout: TabLayout
     private lateinit var viewPager: ViewPager
+    private lateinit var binding: FragmentHomeBinding
     private val viewModel: MovieListViewModel by lazy {
         ViewModelProviders.of(activity!!).get(MovieListViewModel::class.java)
     }
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         showLoading()
         setupObservers()
-        findViews(view)
         setupViewPager()
-        val tab1 = tabLayout.getTabAt(MovieTabs.TAB_POPULAR.tabId)
+        val tab1 = binding.appBar.tabLayout.getTabAt(MovieTabs.TAB_POPULAR.tabId)
         tab1?.select()
     }
 
@@ -48,15 +50,9 @@ class HomeFragment : BaseFragment() {
         })
     }
 
-
-    private fun findViews(view: View) {
-        tabLayout = view.findViewById(R.id.tabLayout)
-        viewPager = view.findViewById(R.id.viewPager)
-    }
-
     private fun setupViewPager() {
-        viewPager.adapter = ViewPagerAdapter(fragmentManager!!)
-        viewPager.offscreenPageLimit = 3
-        tabLayout.setupWithViewPager(viewPager)
+        binding.viewPager.adapter = ViewPagerAdapter(fragmentManager!!)
+        binding.viewPager.offscreenPageLimit = 3
+        binding.appBar.tabLayout.setupWithViewPager(binding.viewPager)
     }
 }
